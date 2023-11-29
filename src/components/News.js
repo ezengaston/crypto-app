ba4d93476655328b8fb68988c912cce415319dab
Ezen <82026690+ezengaston@users.noreply.github.com>
2023-11-29 21:22:32 +0000

    Changed news api

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
 
@@ -12,6 +12,7 @@ const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 export default function News({ simplified }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
   const { data } = useGetCryptosQuery(100);
 
@@ -21,7 +22,19 @@ export default function News({ simplified }) {
     count,
   });
 
-  if (!cryptoNews?.value) return <Loader />;
+  useEffect(() => {
+    if (cryptoNews?.news) {
+      setIsLoading(false);
+    }
+  }, [cryptoNews]);
+
+  const renderNotFound = () => {
+    if (cryptoNews?.news?.length > 0) return null;
+
+    return <Title className="not-found">Not Found</Title>;
+  };
+
+  if (isLoading) return <Loader />;
 
   return (
     <Row gutter={[24, 24]}>
@@ -32,7 +45,10 @@ export default function News({ simplified }) {
             className="select-news"
             placeholder="Select a Crypto"
             optionFilterProp="children"
-            onChange={(value) => setNewsCategory(value)}
+            onChange={(value) => {
+              setIsLoading(true);
+              setNewsCategory(value);
+            }}
             filterOption={(input, option) =>
               option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
@@ -46,46 +62,43 @@ export default function News({ simplified }) {
           </Select>
         </Col>
       ) : null}
-      {cryptoNews?.value?.map((news, index) => (
+      {cryptoNews?.news?.map((news, index) => (
         <Col xs={24} sm={12} lg={8} key={index}>
           <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
                 <Title className="news-title" level={4}>
-                  {news.name}
+                  {news.title}
                 </Title>
                 <img
-                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  src={news?.image || demoImage}
                   alt="news"
                   style={{ maxWidth: "200px", maxHeight: "100px" }}
                 />
               </div>
               <p>
-                {news.description > 100
-                  ? `${news.description.substring(0, 100)}...`
-                  : news.description}
+                {news.body > 100
+                  ? `${news.body.substring(0, 100)}...`
+                  : news.body}
               </p>
               <div className="provider-container">
                 <div>
                   <Avatar
                     src={
-                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      news?.provider?.[0]?.image?.thumbnail?.contentUrl ||
                       demoImage
                     }
                     alt="news"
                   />
-                  <Text className="provider-name">
-                    {news.provider[0]?.name}
-                  </Text>
+                  <Text className="provider-name">{news.source}</Text>
                 </div>
-                <Text>
-                  {moment(news.datePublished).startOf("ss").fromNow()}
-                </Text>
+                <Text>{moment(news.date).startOf("ss").fromNow()}</Text>
               </div>
             </a>
           </Card>
         </Col>
       ))}
+      <Col span={24}>{renderNotFound()}</Col>
     </Row>
   );
 }
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,29 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
-  "x-bingapis-sdk": "true",
-  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "content-type": "application/json",
+  "x-rapidapi-host": "google-api31.p.rapidapi.com",
   "x-rapidapi-key": process.env.REACT_APP_NEWS,
 };
 
-const baseUrl = "https://bing-news-search1.p.rapidapi.com";
-
-function createRequest(url) {
-  return {
-    url,
-    headers: cryptoNewsHeaders,
-  };
-}
+const baseUrl = "https://google-api31.p.rapidapi.com";
 
 export const cryptoNewsApi = createApi({
   reducerPath: "cryptoNewsApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) =>
-        createRequest(
-          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
-        ),
+      query: ({ newsCategory, count }) => ({
+        headers: cryptoNewsHeaders,
+        method: "POST",
+        body: {
+          text: newsCategory,
+          region: "wt-wt",
+          max_results: count,
+        },
+      }),
     }),
   }),
 });
